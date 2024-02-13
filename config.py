9acfa082152035b1e8e178a41e99b3a45c20a944
bipul1010 <bipulvaibhav10@gmail.com>
2024-02-13 14:47:35 +0530

    make seq_len same as max length

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ def get_config():
         "batch_size": 10,
         "num_epochs": 100,
         "lr": 10**-4,
-        "seq_len": 300,
+        "seq_len": 350,
         "d_model": 512,
         "base_path": "/Users/bipulvaibhav/Documents/AI/openai/transformer_attention",
         "datasource": "opus_books",
